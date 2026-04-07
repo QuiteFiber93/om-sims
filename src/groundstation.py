e1d26e0cd44c40f9967fed7f2642ac627b2ab010
Dylan D <djd1494@gmail.com>
2026-04-07 13:53:21 -0400

    StateDef in Measurement

--- src/groundstation.py
+++ src/groundstation.py
@@ -1,11 +1,7 @@
 import numpy as np
 from src.body import CelestialBody, Spacecraft
+from src.measurements import Measurement, MeasurementModel
 
-
-# TODO: Try to use spice kernel for ground station and use numpy based methods if kernels not available
-#       -> Have an object variable which determines whether or not a spice kernel is loaded for the ground station
-#       -> Reference this variable in all functions. If loaded, can use purely spice kernels for transformations
-#       -> If not loaded, use spice kernels for transformations to body frame and math for body -> ground station.
 class GroundStation:
     """Class containing useful behaviors for ground station calculations
     """
@@ -14,33 +10,10 @@ class GroundStation:
                  frame: str, 
                  lat: float, 
                  lon: float, 
-                 altitude: float, 
-                 body: CelestialBody, 
+                 alt: float, 
                  el_mask: float, 
-                 noise: dict, 
-                 id: float = None,
-                 clock_bias: float = 0,
-                 use_spice_station = True
+                 observations: MeasurementModel, 
+                 id: int = None, 
+                 clock_bias: float = 0.0,
                  ):
-
-        self.name = name
-        self.frame = frame
-        self.lat = lat
-        self.lon = lon
-        self.altitude = altitude
-        self.body = body
-        self.el_mask = el_mask
-        self.clock_bias = clock_bias
-        
-        # TODO: FIX ID LOGIC, Make it more similar to the SC id logic
-        self.id = int( str(self.body.id) + str(id) )
-        self.use_spice_station = use_spice_station
-        
-    def true_range(self, r: np.ndarray) -> np.ndarray:
-        pass
-    
-    def true_rangerate(self, r: np.ndarray, v: np.ndarray) -> np.ndarray:
-        pass
-    
-    def measurements(self, et, r, v, observations: list[str]):
         pass
--- src/measurements.py
+++ src/measurements.py
@@ -1,9 +1,11 @@
 import numpy as np
 
+from src.state import StateDefinition
+
 class Measurement:
     """Base class for Measurement values such as range, range rate, altitude, and azimuth
     """
-    def __init__(self, bias: float | np.array, sigma: float | np.array):
+    def __init__(self, statedef: StateDefinition, bias: float | np.array, sigma: float | np.array):
         pass
     
     def h(self):
@@ -16,13 +18,54 @@ class Measurement:
         return 0
 
 class Range(Measurement):
-    pass
+    def __init__(self, statedef: StateDefinition, bias, sigma):
+        self.bias = bias
+        self.sigma = sigma
+        
+        if not statedef.has('position'):
+            raise ValueError("Position indices not included in StateDefinition: statedef.")
+        
+        self.pos_idx = statedef['position']
+        
+    def h(self, et: float | np.ndarray, state: np.ndarray, station_pos: np.ndarray):
+        r = state[self.pos_idx]
+        return np.linalg.norm(r - station_pos)
+        
+    def H(self, et: float | np.ndarray, r: np.ndarray):
+        pass
 
 class RangeRate(Measurement):
-    pass
+    def __init__(self, statedef: StateDefinition, bias, sigma):
+        self.bias = bias
+        self.sigma = sigma
+        
+        if not statedef.has('position'):
+            raise ValueError("Position indices not included in StateDefinition: statedef.")
+        
+        if not statedef.has('velocity'):
+            raise ValueError('Velocity indices not included in StateDefinition: statedef.')
+        
+        self.pos_idx = statedef['position']
+        self.vel_idx = statedef['velocity']
+        
+    def h(self, et: float | np.ndarray, state: np.ndarray, station_pos: np.ndarray):
+        r = state[self.pos_idx]
+        v = state[self.vel_idx]
+        
+        return  r @ v / np.linalg.norm(r - station_pos)
 
 class PositionAngles(Measurement):
-    pass
+    def __init__(self, statedef: StateDefinition, bias, sigma):
+        self.bias = bias
+        self.sigma = sigma
+        
+        if not statedef.has('position'):
+            raise ValueError("Position indices not included in StateDefinition: statedef.")
+        
+        self.pos_idx = statedef['position']
+        
+    def h(self, et: float | np.ndarray, r: np.ndarray, station_pos: np.ndarray):
+        pass
 
 class MeasurementModel:
     def __init__(self, measurements: list[Measurement]):
